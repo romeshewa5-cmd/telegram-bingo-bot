976f0c130b61acc3303d3666fba7dd2ea602a7d4
romeshewa5-cmd <romeshewa5@gmail.com>
2026-01-23 08:25:05 -0800

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,11 +1,6 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-
-# Serve WebApp
-app.mount("/webapp", StaticFiles(directory="webapp", html=True), name="webapp")
-
 
 @app.get("/")
 def root():
